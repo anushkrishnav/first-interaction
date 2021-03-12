7a812600cf6eeb6c200f247318b0cca993d1fb06
A N U S H <54374648+anushkrishnav@users.noreply.github.com>
2021-03-12 15:51:08 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -38,23 +38,6 @@ async function run() {
     const sender: string = context.payload.sender!.login;
     const issue: {owner: string; repo: string; number: number} = context.issue;
     let firstContribution: boolean = false;
-    if (isIssue) {
-      firstContribution = await isFirstIssue(
-        client,
-        issue.owner,
-        issue.repo,
-        sender,
-        issue.number
-      );
-    } else {
-      firstContribution = await isFirstPull(
-        client,
-        issue.owner,
-        issue.repo,
-        sender,
-        issue.number
-      );
-    }
     // Do nothing if no message set for this type of contribution
     const message: string = isIssue ? issueMessage : prMessage;
     if (!message) {
